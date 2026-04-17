a8b075d817d034d604e8d416ba20fb4df18e2010
Kenedi Nádja <nadja.kenedi@gmail.com>
2026-04-17 21:25:35 +0200

    corrected palindrom (task 1)

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -5,6 +5,7 @@ def make_palindrom(half,even_lengt):
         return half + half[::-1]
     else:
         return half+half[:-1][::-1]    
+    
 def next_magic_num(value):
     length = len(value)
     cut = (length+1)//2
@@ -17,7 +18,7 @@ def next_magic_num(value):
         return first_attempt        
 
     converted_first_half = int(first_half)+1
-    new_first_half = str(converted_first_half+1)
+    new_first_half = str(converted_first_half)
 
     if len(new_first_half)>len(first_half):
         return "1"+"0"*(length-1)+"1"
@@ -28,8 +29,17 @@ from pathlib import Path
 
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    input_file = Path("input.txt")
+    data = input_file.read_text(encoding="utf-8")
+    for line in data.split():
+        line = line.strip()
+    for line in data.splitlines():
+        num_str = line.strip() 
+        if num_str and num_str.isdigit():
+            print(next_magic_num(num_str))
+        else: continue    
+
+
 
 
 if __name__ == "__main__":
